405452505095754f0f6a6cf20e59168b57475820
Sanchari Saha <ssanchari84@gmail.com>
2026-09-19 11:28:22 +0530

    Add: fruit-into-baskets - submission-0

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        left = 0
+        freq ={}
+        max_len = 0
+        for i in range(len(fruits)) :
+            freq[fruits[i]] = freq.get(fruits[i], 0) + 1
+            
+            while len(freq) > 2: # invalid window
+                freq[fruits[left]] -= 1
+                if freq[fruits[left]] == 0:
+                    del freq[fruits[left]]
+                left +=1
+            max_len = max(max_len, i-left+1)
+        return max_len
+
+        
